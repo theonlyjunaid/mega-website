64c68c1f3d18a725cf9b4f58e437ac567f162971
sahil <chhabrasahil20@gmail.com>
2024-07-16 21:21:45 +0530

    commit change favicon

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -13,7 +13,7 @@ function AllPosts() {
     }, [])
     
   return (
-    <div className='w-full py-8'>
+    <div className='w-full min-h-screen py-8 bg-gradient-to-r from-gray-800 via-gray-900 to-black'>
         <Container>
             <div className='flex flex-wrap'>
                 {posts.map((post) => (
